0b79d003d0072ee032027f3125ac7234397f8b69
lapots <sebastrident@gmail.com>
2018-03-30 21:42:10 +0300

    Left Hand Side implementation

---
+++ src/main/java/com/lapots/breed/rule/generator/template/populator/LeftHandSidePopulator.java
@@ -0,0 +1,86 @@
+package com.lapots.breed.rule.generator.template.populator;
+
+import com.lapots.breed.rule.domain.Binding;
+import com.lapots.breed.rule.domain.Condition;
+import com.lapots.breed.rule.domain.DataRule;
+import com.lapots.breed.rule.domain.WhenBlock;
+import com.lapots.breed.rule.generator.template.populator.api.AbstractPopulator;
+import com.lapots.breed.rule.generator.template.populator.api.ITemplatePopulator;
+import com.lapots.breed.rule.internal.ConfigurationHolder;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Populates lhs in template.
+ */
+public class LeftHandSidePopulator extends AbstractPopulator {
+    /**
+     * Constructor.
+     *
+     * @param next next populator in chain
+     */
+    public LeftHandSidePopulator(ITemplatePopulator next) {
+        super(next);
+    }
+
+    @Override
+    protected Map<String, Object> internalPopulate(Map<String, Object> templateData, DataRule src) {
+        WhenBlock when = src.getExecution().getWhen();
+
+        Map<String, String> conditions = when.getConditions()
+                .stream()
+                .collect(Collectors.toMap(Condition::getConditionId, this::transformCondition));
+
+        // TODO:implement for multiple bindings aka () && () and so on.
+        List<String> expressions = when.getBindings()
+                .stream()
+                .collect(Collectors.toMap(Binding::getType, Function.identity()))
+                .entrySet()
+                .stream()
+                .map(bindingEntry -> formExpression(conditions, bindingEntry.getValue()))
+                .collect(Collectors.toList());
+
+        templateData.put("lhs", expressions.get(0));
+        return templateData;
+    }
+
+    /**
+     * Converts condition [id, type, left, right] to [left + type + right] string.
+     * @param condition condition
+     * @return string of condition
+     */
+    private String transformCondition(Condition condition) {
+        StringBuilder code = new StringBuilder();
+
+        String leftBlock = condition.getLeft().getCode();
+        code.append(leftBlock);
+
+        String operator = condition.getConditionType();
+        operator = ConfigurationHolder.findByKey("conditions." + operator);
+        code.append(" ").append(operator).append(" ");
+
+        String rightBlock = condition.getRight().getCode();
+        code.append(rightBlock);
+        return code.toString();
+    }
+
+    /**
+     * Forms expression from left and rights side using binding as intermediate.
+     * @param conditions conditions
+     * @param binding binding element
+     * @return expression
+     */
+    private String formExpression(Map<String, String> conditions, Binding binding) {
+        String operator = " " + ConfigurationHolder.findByKey("bindings." + binding.getType()) + " ";
+        return conditions.entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .filter(entry -> binding.getConditions().contains(entry.getKey()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.joining(operator));
+    }
+}
---
+++ src/test/groovy/com/lapots/breed/rule/generator/template/populator/LeftHandSidePopulatorTestSpec.groovy
@@ -0,0 +1,58 @@
+package com.lapots.breed.rule.generator.template.populator
+
+import com.lapots.breed.rule.domain.Binding
+import com.lapots.breed.rule.domain.Condition
+import com.lapots.breed.rule.domain.ConditionSide
+import com.lapots.breed.rule.domain.DataRule
+import com.lapots.breed.rule.domain.ExecutionRule
+import com.lapots.breed.rule.domain.WhenBlock
+import com.lapots.breed.rule.internal.ConfigurationHolder
+import org.mockito.Mockito
+
+class LeftHandSidePopulatorTestSpec extends PopulatorTestSpecification {
+
+    def "should create left hand side"() {
+        setup:
+            def src = new DataRule(
+                    execution: new ExecutionRule(
+                            when: new WhenBlock(
+                                    conditions: [
+                                            new Condition(
+                                                    conditionId: "cond_1",
+                                                    conditionType: "less_than",
+                                                    left: new ConditionSide(
+                                                            code: "5"
+                                                    ),
+                                                    right: new ConditionSide(
+                                                            code: "15"
+                                                    )
+                                            ),
+                                            new Condition(
+                                                    conditionId: "cond_2",
+                                                    conditionType: "more_than",
+                                                    left: new ConditionSide(
+                                                            code: "10"
+                                                    ),
+                                                    right: new ConditionSide(
+                                                            code: "15"
+                                                    )
+                                            )
+                                    ],
+                                    bindings: [
+                                            new Binding(
+                                                    type: "and",
+                                                    conditions: [ "cond_1", "cond_2" ]
+                                            )
+                                    ]
+                            )
+                    )
+            )
+            def populator = new LeftHandSidePopulator(null)
+        when:
+            Mockito.when(ConfigurationHolder.findByKey("bindings.and")).thenReturn("&&")
+            Mockito.when(ConfigurationHolder.findByKey("conditions.less_than")).thenReturn("<")
+            Mockito.when(ConfigurationHolder.findByKey("conditions.more_than")).thenReturn(">")
+        then:
+            assert ["lhs" : "5 < 15 && 10 > 15"] == populator.populate([:], src)
+    }
+}
